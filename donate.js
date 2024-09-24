f0742ad4b347af5227c88eb2e6a8770b5c443c10
web-sakib <sakibsadman9876@gmail.com>
2024-09-25 04:06:46 +0600

    almost done

--- donate.js
+++ donate.js
@@ -17,32 +17,90 @@ document.getElementById('donate-btn1').addEventListener('click', function () {
   const mainBalance = getTextById('main-balance');
   const cardBalance = getTextById('card1-added-balance')
 
-  if(inputAmount > 0 && !isNaN(inputAmount) && inputAmount <= mainBalance){
+  if (inputAmount > 0 && !isNaN(inputAmount) && inputAmount <= mainBalance) {
     const afterDonate = mainBalance - inputAmount;
     document.getElementById('main-balance').innerText = afterDonate
     const afterDonate2 = inputAmount + cardBalance;
     document.getElementById('card1-added-balance').innerText = afterDonate2;
+    document.getElementById('card1-input').value = ' ';
+    alert('You Have donated successfully')
   }
   else {
-    alert('Please input a valid ammount')
+    alert('Please input a valid amount')
   }
 
-  // for transaction 
+  // for history transaction 
   const transaction = document.createElement('div')
-  transaction.innerHTML = `
-  
+  transaction.innerHTML = ` 
   <div class="border p-5 container mx-auto rounded-lg shadow-md">
-      <h3 class="text-lg font-semibold mb-2"> ${inputAmount} Taka is Donated Flood at Noakhali, Bangladesh</h3>
+      <h3 class="text-lg font-semibold mb-2"> ${inputAmount} Taka is Donated for flood at Noakhali, Bangladesh</h3>
       <p> ${new Date().toLocaleString()} </p>
     </div>
-  
   `
-
-  // document.getElementById('history-section').appendChild(transaction) // system 1
-  document.getElementById('history-section').prepend(transaction) // system 1
-  
-
+  document.getElementById('history-section').prepend(transaction)
 
 })
 
+
+// Card 2 - Donate button with addEventListener
+document.getElementById('donate-btn2').addEventListener('click', function () {
+
+  const inputAmount = getInputValueById('card2-input');
+  const mainBalance = getTextById('main-balance');
+  const cardBalance = getTextById('card2-added-balance')
+
+  if (inputAmount > 0 && !isNaN(inputAmount) && inputAmount <= mainBalance) {
+    const afterDonate = mainBalance - inputAmount;
+    document.getElementById('main-balance').innerText = afterDonate
+    const afterDonate2 = inputAmount + cardBalance;
+    document.getElementById('card2-added-balance').innerText = afterDonate2;
+    document.getElementById('card2-input').value = ' ';
+    alert('You Have donated successfully')
+  }
+  else {
+    alert('Please input a valid amount')
+  }
+
+  // for history transaction 
+  const transaction = document.createElement('div')
+  transaction.innerHTML = ` 
+  <div class="border p-5 container mx-auto rounded-lg shadow-md">
+      <h3 class="text-lg font-semibold mb-2"> ${inputAmount} Taka is Donated for flood relief in Feni, Bangladesh</h3>
+      <p> ${new Date().toLocaleString()} </p>
+    </div>
+  `
+  document.getElementById('history-section').prepend(transaction)
+
+})
+
+// Card 3 - Donate button with addEventListener
+document.getElementById('donate-btn3').addEventListener('click', function () {
+
+  const inputAmount = getInputValueById('card3-input');
+  const mainBalance = getTextById('main-balance');
+  const cardBalance = getTextById('card3-added-balance')
+
+  if (inputAmount > 0 && !isNaN(inputAmount) && inputAmount <= mainBalance) {
+    const afterDonate = mainBalance - inputAmount;
+    document.getElementById('main-balance').innerText = afterDonate
+    const afterDonate2 = inputAmount + cardBalance;
+    document.getElementById('card3-added-balance').innerText = afterDonate2;
+    document.getElementById('card3-input').value = ' ';
+    alert('You Have donated successfully')
+  }
+  else {
+    alert('Please input a valid amount')
+  }
+
+  // for history transaction 
+  const transaction = document.createElement('div')
+  transaction.innerHTML = ` 
+  <div class="border p-5 container mx-auto rounded-lg shadow-md">
+      <h3 class="text-lg font-semibold mb-2"> ${inputAmount} Taka is Donated for injured in the Quota Movement</h3>
+      <p> ${new Date().toLocaleString()} </p>
+    </div>
+  `
+  document.getElementById('history-section').prepend(transaction)
+
+})
 
